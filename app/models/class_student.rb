b034bb286d28846155ee9d4bdf84cf1873cae147
Cory <corystephens012@gmail.com>
2021-10-22 14:25:27 -0400

    98% functional app; check sessions/auth and apply validations to prevent duplicates

--- app/models/class_student.rb
+++ app/models/class_student.rb
@@ -1,4 +1,6 @@
 class ClassStudent < ApplicationRecord
   belongs_to :student
   belongs_to :gym_class
+
+#  validates :student_id, uniqueness: :true
 end
--- app/models/class_workout.rb
+++ app/models/class_workout.rb
@@ -1,4 +1,6 @@
 class ClassWorkout < ApplicationRecord
   belongs_to :workout_plan
   belongs_to :gym_class
+
+  # validates :workout_plan, uniqueness: true
 end
